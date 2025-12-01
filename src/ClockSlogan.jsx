99a65cd20e315f36a969fb566b84b8306b5bd1f0
Ashish Kumar Singh <ashishkumar77393@gmail.com>
2025-12-01 23:18:47 +0530

    Update ClockSlogan.jsx

--- src/ClockSlogan.jsx
+++ src/ClockSlogan.jsx
@@ -1,6 +1,17 @@
-function ClockSlogan(){
-  return(
-    <p style={{"fontSize":"30px",'color':'green'}}>This is a clock Show the Time in Bharat At All time  </p>
-  )
+function ClockSlogan() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <p style={{ fontSize: "30px", color: "green" }}>
+        This is a clock Show the Time in Bharat At All time  
+      </p>
+
+      <img 
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Analog_watch_face.svg/2048px-Analog_watch_face.svg.png" 
+        alt="Bharat Clock Watch" 
+        width="200"
+      />
+    </div>
+  );
 }
+
 export default ClockSlogan;
